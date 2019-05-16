36edf24b64862062ebe46dcb73f59af87a0db997
Masato Yamashita <masatoyamashita@rrcs-172-254-99-69.nyc.biz.rr.com>
2019-05-17 06:50:26 +0900

    Deleted unnecesary folders and files.

--- model/location_weather.rb
+++
@@ -1,5 +0,0 @@
-class LocationWeather
-  attr_accessor :today_weather
-  attr_accessor :today_date
-  attr_accessor :city_name
-end
--- view/weather_display.rb
+++
@@ -1,7 +0,0 @@
-class WeatherDisplay
-  def display(city_name, weather)
-    print "City: #{city_name}, "
-    print "Weather: #{weather}"
-    puts ''
-  end
-end
