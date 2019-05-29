7f4cb0c0a3c379df9afe7fb0333c9abd25d2fe49
Masato Yamashita <m.yamashii@gmail.com>
2019-05-29 13:18:55 +0900

    Created kanto page to display weather in kanto area #15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
+  get 'area/kanto' => 'area/kanto#index'
 
   resources :articles
 
---
+++ test/controllers/area/kanto_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Area::KantoControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
