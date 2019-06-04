67a78186de1bafe298a70910810d069db11e60dc
Masato Yamashita <m.yamashii@gmail.com>
2019-06-04 20:48:38 +0900

    Implemented hokkaido page to display weather.#23

--- app/controllers/area/hokkaido_controller.rb
+++ app/controllers/area/hokkaido_controller.rb
@@ -1,4 +1,11 @@
 class Area::HokkaidoController < ApplicationController
   def index
+    creator = URLCreator.new
+    reader = DataReader.new
+    weather_collection = WeatherCollection.new(creator, reader)
+    location_weather_list = weather_collection.get_location_weather_in_area('北海道')
+
+    @location_weather_arr = location_weather_list.get_list
+    @location_weather_arr.reverse!
   end
 end
