cf6011ac5af16aa566a7bc71677746e1e5174e73
Masato Yamashita <m.yamashii@gmail.com>
2019-08-08 23:00:22 +0900

    Vue内でログインできる機能を作りました。#48

--- app/javascript/packs/components/signin-template.vue
+++ app/javascript/packs/components/signin-template.vue
@@ -1,35 +1,82 @@
 <template>
   <div id="signin-template">
     <div>
-      <h5>ユーザ名:</h5><input type="text" placeholder="xxxxxx" v-model="userName">
+      <label>ユーザ名:</label>
+      <input type="text" placeholder="xxxxxx" v-model="user.name">
     </div>    
     <div>
-      <h5>メールアドレス</h5>
-      <input type="email" placeholder="xxx@yyy.zzz" v-model="email">
+      <label>メールアドレス:</label>
+      <input type="email" placeholder="xxx@yyy.zzz" v-model="user.email">
     </div>
     <div>
-      <h5>パスワード</h5>
-      <input type="password" placeholder="xxxxxxxx" v-model="password">
+      <label>パスワード:</label>
+      <input type="password" placeholder="xxxxxxxx" v-model="user.password">
     </div>
-    <button @click="signin()">サインイン</button>
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+    <button @click="createUser">サインイン</button>
   </div>
 </template>
 
 <script>
 export default {
-    el: "login",
-    data: function(){
-      return {
-        userName: '',
-        email: '',
-        password: ""
-      }
+  el: "login",
+  data: function　() {
+    return {
+      sending: false,
+      user: this.defaultUser(),
+      error: null
+    }
+  },
+
+  created: function () {
+  },
+
+  methods: {
+    postUser : function (params, callback) {
+      setTimeout(function(){
+        callback(null, params)
+      }, 1000)
     },
 
-    methods: {
-      signin: function() {
-        // auth.login(this.userid, this.password);
+    defaultUser: function () {
+      return {
+        name: "",
+        email: "",
+        password: "",
       }
     },
-};
+    createUser: function () {
+      // 入力パラメータのバリデーション
+      if (this.user.name.trim() === "") {
+        this.error = "ユーザ名は必須です";
+        return;
+      }
+      if (this.user.email.trim() === "") {
+        this.error = "メールアドレスは必須です";
+        return;
+      }
+      if (this.user.password.trim() === "") {
+        this.error = "パスワードは必須です";
+        return;
+      }
+
+      this.postUser(this.user, (function(err, user) {
+        this.sending = false;
+        if (err){
+          this.error = err.toString();
+        } else {
+          this.error = null,
+          alert("新規ユーザが登録されました。ユーザ名 : " + this.user.name);
+          this.user = this.defaultUser();
+          
+          //トップページにもどる
+          this.$router.push("/");
+        }
+
+      }).bind(this))
+    }
+  }
+}
 </script>
