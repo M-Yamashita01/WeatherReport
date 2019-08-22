0435982123e03ff517cccc05dc4240cfe01eed87
Masato Yamashita <m.yamashii@gmail.com>
2019-08-22 22:50:34 +0900

    Added <script src=・・・> in application.html.erb to enable to display toggle menu #58

--- app/javascript/packs/components/shared/header.vue
+++ app/javascript/packs/components/shared/header.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-info">
-      <router-link to="/">WeatherReport</router-link>
-      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigationBar" aria-controls="navigationBar" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-default fixed-top navbar-dark bg-primary">
+      <router-link to="/" class="navbar-brand text-white">WeatherReport</router-link>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigationBar" aria-controls="navigationBar" aria-expanded="true" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navigationBar">
+      <div class="navbar-collapse collapse" id="navigationBar">
         <ul class="navbar-nav">
-          <li class="nav-item"><router-link to="#">雨雲の動き</router-link></li>
-          <li class="nav-item"><router-link to="#">地震情報</router-link></li>
-          <li class="nav-item"><router-link to="/login">ログイン</router-link></li>
-          <li class="nav-item"><router-link to="/logout">ログアウト</router-link></li>
+          <li class="nav-item"><router-link to="#" class="text-white">雨雲の動き</router-link></li>
+          <li class="nav-item"><router-link to="#" class="text-white">地震情報</router-link></li>
+          <li class="nav-item"><router-link to="/login" class="text-white">ログイン</router-link></li>
+          <li class="nav-item"><router-link to="/logout" class="text-white">ログアウト</router-link></li>
         </ul>
       </div>
     </nav>
