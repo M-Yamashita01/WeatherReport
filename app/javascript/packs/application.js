f27f979e90565bae7f79bfb6115935780169b423
MYamashii <m.yamashii@gmail.com>
2019-08-23 01:10:56 +0900

    Added <script src=・・・> in application.html.erb to enable to display toggle menu #58 (#59)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,13 +17,18 @@
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import BootstrapVue from 'bootstrap-vue';
 import store from '../store/index';
 import router from '../routes';
 import App from '../app';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 // import '../../assets/stylesheets/application';
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 document.addEventListener('DOMContentLoaded', () => {
 	new Vue({
