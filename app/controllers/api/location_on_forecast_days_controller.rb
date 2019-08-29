f99db6ec4abfc148976669f192c62b32e2d97a23
Masato Yamashita <m.yamashii@gmail.com>
2019-08-29 23:10:30 +0900

    日付ボタンで天気の表示を変えられるようにしました。#66

--- app/controllers/api/location_on_forecast_days_controller.rb
+++ app/controllers/api/location_on_forecast_days_controller.rb
@@ -1,7 +1,6 @@
 class Api::LocationOnForecastDaysController < ApplicationController
   def index
-    @location_on_forecast = LocationOnForecastDay.joins(:location, :weather).where('locations.main_city_flag = ?', params[:main_city_flag]).select('location_on_forecast_days.*, locations.*, weathers.*')
-    #@location_on_forecast = LocationOnForecastDay.joins(:location, :weather).select('location_on_forecast_days.*, locations.*, weathers.*')
+    @location_on_forecast = LocationOnForecastDay.joins(:location, :weather).where(['locations.main_city_flag = ? and location_on_forecast_days.forecast_day = ?', params[:main_city_flag], params[:date]]).select('location_on_forecast_days.*, locations.*, weathers.*')
   end
 
   def create
