09ecb80ccfc7d9ad520bcc90a6836b330c6beaef
Masato Yamashita <m.yamashii@gmail.com>
2019-09-03 07:41:35 +0900

    メニューバーの色が変わっていない問題を修正#71

--- app/javascript/packs/components/shared/header.vue
+++ app/javascript/packs/components/shared/header.vue
@@ -1,30 +1,26 @@
 <template>
   <div>
-    <nav class="navbar fixed-top navbar-default navbar-toggleable-md scrolling-navbar double-nav navbar-light">
-      <div class="float-left">
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigationBar" aria-controls="navigationBar" aria-expanded="true" aria-label="Toggle navigation" :class="{'is-active': $store.state.isMenuActive}">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="navbar-collapse collapse" id="navigationBar">
-          <ul class="navbar-nav">
-            <li class="nav-item"><router-link to="#" data-toggle="collapse" data-target=".navbar-collapse.show">雨雲の動き</router-link></li>
-            <li class="nav-item"><router-link to="#" data-toggle="collapse" data-target=".navbar-collapse.show">地震情報</router-link></li>
-          </ul>
-        </div>
-      </div>
-      <div class="breadcrumb-dn mr-auto">
-        <ul>Weather Report</ul>
-      </div>
-      <ul class="nav navbar-nav nav-flex-icons ml-auto">
+    <nav class="navbar fixed-top navbar-default navbar-toggleable-md bs-navbar navbar-dark">
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigationBar" aria-controls="navigationBar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <router-link to="/" class="navbar-brand">Weather Report</router-link>
+      <ul class="nav navbar-nav nav-flex-icons ml-auto text-white">
         <li class="nav-item" v-if="!logined"><router-link to="/login" data-toggle="collapse" data-target=".navbar-collapse.show">ログイン</router-link></li>
         <li class="nav-item" v-if="logined">
           <div>
             {{ userName }}
-            <router-link to="/logout" data-toggle="collapse" data-target=".navbar-collapse.show">ログアウト</router-link>
+            <router-link to="/logout" data-toggle="collapse" data-target=".navbar-collapse.show" class="text-white">ログアウト</router-link>
           </div>
         </li> 
       </ul>
+
+      <div class="navbar-collapse collapse" id="navigationBar">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item"><router-link to="#" data-toggle="collapse" data-target=".navbar-collapse.show" class="text-white">雨雲の動き</router-link></li>
+          <li class="nav-item"><router-link to="#" data-toggle="collapse" data-target=".navbar-collapse.show" class="text-white">地震情報</router-link></li>
+        </ul>
+      </div>
     </nav>
   </div>
 </template>
