3d8f86ded9ae3b872d9fba3e1c938a18ccc279a1
Masato Yamashita <m.yamashii@gmail.com>
2019-09-09 22:59:49 +0900

    検索する値があればwhere句に組み込むよう修正しました。#82

--- app/models/location_on_forecast_day.rb
+++ app/models/location_on_forecast_day.rb
@@ -1,4 +1,20 @@
 class LocationOnForecastDay < ApplicationRecord
   belongs_to :location, foreign_key: :location_id, primary_key: :location_id
   belongs_to :weather, foreign_key: :weather_id, primary_key: :weather_id
+
+  scope :search, lambda { |params|
+    search_by_main_city_flag(params[:main_city_flag]).search_by_date(params[:date])
+  }
+
+  scope :search_by_main_city_flag, lambda { |main_city_flag|
+    return all if main_city_flag.blank?
+
+    where('locations.main_city_flag = ?', main_city_flag)
+  }
+
+  scope :search_by_date, lambda { |date|
+    return all if date.blank?
+
+    where('location_on_forecast_days.forecast_day = ?', date)
+  }
 end
