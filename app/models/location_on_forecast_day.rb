178ba023888e800aeccb857a28493e24a7ec1313
Masato Yamashita <m.yamashii@gmail.com>
2019-09-10 01:50:36 +0900

    Rails APIサーバーに渡すwhere句の引数の並べ方を整理しました#82

--- app/models/location_on_forecast_day.rb
+++ app/models/location_on_forecast_day.rb
@@ -3,19 +3,19 @@ class LocationOnForecastDay < ApplicationRecord
   belongs_to :weather, foreign_key: :weather_id, primary_key: :weather_id
 
   scope :search, lambda { |params|
-    search_by_main_city_flag(params[:main_city_flag]).search_by_date(params[:date]).search_by_less_than_longitude(params[:longitude_max]).search_by_more_than_longitude(params[:longitude_min]).search_by_less_than_latitude(params[:latitude_max]).search_by_more_than_latitude(params[:latitude_min])
-  }
-
-  scope :search_by_main_city_flag, lambda { |main_city_flag|
-    return all if main_city_flag.blank?
-
-    where('locations.main_city_flag = ?', main_city_flag)
+    search_by_date(params[:date]).search_by_main_city_flag(params[:main_city_flag]).search_by_less_than_longitude(params[:longitude_max]).search_by_more_than_longitude(params[:longitude_min]).search_by_less_than_latitude(params[:latitude_max]).search_by_more_than_latitude(params[:latitude_min])
   }
 
   scope :search_by_date, lambda { |date|
     return all if date.blank?
 
     where('location_on_forecast_days.forecast_day = ?', date)
+  }
+
+  scope :search_by_main_city_flag, lambda { |main_city_flag|
+    return all if main_city_flag.blank?
+
+    where('locations.main_city_flag = ?', main_city_flag)
   }
 
   scope :search_by_less_than_longitude, lambda { |longitude|
