446c28bcf69c35adae345f39afabc59ff5c24bff
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2019-09-14 12:15:12 +0900

    Deleted unnecessary imports.

--- app/javascript/packs/components/MainContents.vue
+++ app/javascript/packs/components/MainContents.vue
@@ -6,15 +6,7 @@
 </template>
 
 <script>
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4maps from "@amcharts/amcharts4/maps";
-import am4geodata_japanLow from "@amcharts/amcharts4-geodata/japanLow";
-import axios from "axios";
-import { constants } from "crypto";
-import { circleIn } from "@amcharts/amcharts4/.internal/core/utils/Ease";
-
 import Weathers from "./weathers.js";
-import { setTimeout } from "timers";
 import WeatherMap from "./WeatherMap";
 
 export default {
--- app/javascript/packs/components/WeatherMap.vue
+++ app/javascript/packs/components/WeatherMap.vue
@@ -24,8 +24,6 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4geodata_japanLow from "@amcharts/amcharts4-geodata/japanLow";
 import axios from "axios";
-import { constants } from "crypto";
-import { circleIn } from "@amcharts/amcharts4/.internal/core/utils/Ease";
 
 import { setTimeout } from "timers";
 
