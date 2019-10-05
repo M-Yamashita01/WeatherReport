493a54fba19861d6865b8923a3c6b7e49d6aff9e
Masato Yamashita <m.yamashii@gmail.com>
2019-10-05 09:25:48 +0900

    Added current_weather_data controller class.

---
+++ app/controllers/current_weather_data_controller.rb
@@ -0,0 +1,2 @@
+class CurrentWeatherDataController < ApplicationController
+end
---
+++ test/controllers/current_weather_data_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CurrentWeatherDataControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
