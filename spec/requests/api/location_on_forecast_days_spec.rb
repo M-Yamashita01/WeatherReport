4b3cf2b3316ad579db28101926db731eed18326f
Masato Yamashita <m.yamashii@gmail.com>
2019-10-16 22:47:17 +0900

    Change single quoted strings #110

--- spec/requests/api/location_on_forecast_days_spec.rb
+++ spec/requests/api/location_on_forecast_days_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "LocationOnForecastDays", type: :request do
-  describe "GET /location_on_forecast_days" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'LocationOnForecastDays', type: :request do
+  describe 'GET /location_on_forecast_days' do
+    it 'works! (now write some real specs)' do
       get '/api/location_on_forecast_days'
       expect(response).to have_http_status(200)
     end
