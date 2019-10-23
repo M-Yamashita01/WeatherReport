045dc6e8e93c73953744755a653a0b9f6a545a1d
Masato Yamashita <m.yamashii@gmail.com>
2019-10-24 08:02:06 +0900

    Change proxy path. #131

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,34 +1,8 @@
 module.exports = {
-  pages: {
-    index: {
-      // 最初に実行されるファイル名
-      entry: "src/main.js",
-      // テンプレートファイル
-      template: "public/index.html",
-      // 出力されるファイル名
-      filename: "index.html",
-      // タイトルの設定
-      title: "Weather Report",
-      // チャンクの指定
-      chunks: ["chunk-vendors", "chunk-common", "index"]
-    }
-  },
   devServer: {
-    port: 8080,
-    disableHostCheck: true,
     proxy: {
-      "/api/auth/registrations": {
-        target: "http://0.0.0.0:3000",
-        ws: true,
-        changeOrigin: true
-      },
-      "/api/current_weather_datas": {
-        target: "http://0.0.0.0:3000",
-        ws: true,
-        changeOrigin: true
-      },
-      "/api/main_city_locations": {
-        target: "http://0.0.0.0:3000",
+      "^/api*": {
+        target: "http://server:3000",
         ws: true,
         changeOrigin: true
       }
