6847d62de073ce2b13470249592cd30427487b33
Masato Yamashita <m.yamashii@gmail.com>
2019-11-01 20:03:22 +0900

    Create user model test. #133

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
-  factory :micropost do
-    content { 'MyText' }
-    user { nil }
+  factory :sunny_comment, class: Micropost do
+    content { 'today is sunny.' }
+    created_at { Time.now}
+  end
+
+  factory :cloudy_comment, class: Micropost do
+    content { 'tomorrow is cloudy.' }
+  end
+
+  factory :rainy_comment, class: Micropost do
+    content { 'day after tomorrow is rainy.' }
   end
 end
