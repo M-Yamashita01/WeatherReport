6d72d117118db67e8829ae9fe647c9fcff08fb31
Masato Yamashita <m.yamashii@gmail.com>
2019-11-14 20:38:04 +0900

    Delete style in twitter components.#149

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -5,13 +5,7 @@
       <div class="card-header text-center">Twitter</div>
       <div class="card-body">
         <div>
-          <div
-            class="twitter-widget"
-            style="width:400px;"
-            :index="i"
-            v-for="(tw, i) in twitterIds"
-            :key="tw.title"
-          >
+          <div :index="i" v-for="(tw, i) in twitterIds" :key="tw.title">
             <timeline
               :id="tw"
               :source-type="'profile'"
