51ff1e6f96553d13d0cc54bbafc79248f99220d5
Masato Yamashita <m.yamashii@gmail.com>
2019-11-16 16:17:52 +0900

    Create post modal. in profile page.#149

---
+++ frontend/src/components/CommentModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-modal id="commentModal" @show="resetModal" @hidden="resetModal">
+    <template v-slot:modal-title>
+      コメント
+    </template>
+    <input type="text" class="form-control" v-model="comment" />
+    <template v-slot:modal-footer="{ ok, cancel }">
+      <b-button size="sm" variant="primary" @click="postComment()">
+        投稿
+      </b-button>
+      <b-button size="sm" variant="light" @click="cancel()">
+        キャンセル
+      </b-button>
+    </template>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comment: "",
+      commentState: null
+    };
+  },
+  methods: {
+    postComment() {
+      if (this.comment == "") {
+        return;
+      }
+
+      this.$bvModal.hide("commentModal");
+      this.resetModal();
+    },
+    resetModal() {
+      this.comment = "";
+    }
+  }
+};
+</script>
--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,89 +1,93 @@
 <template>
-  <div class="container-for-admin">
-    <!--Main layout-->
-    <main class="pt-5 mx-lg-5">
-      <div class="container-fluid mt-5">
-        <!--Grid row-->
-        <div class="row wow fadeIn">
-          <!--Grid column-->
-          <div class="col-md-4 mb-4">
-            <!--Card-->
-            <div class="card text-center">
-              <!--Card content-->
-              <div class="card-body">
-                <router-link class="text-body" to="/profile-edit">
-                  <button
-                    type="button"
-                    class="btn btn-sm waves-effect profile-edit"
-                  >
-                    編集
-                  </button>
-                </router-link>
-                <img :src="assetsImage" width="100" height="100" />
-                <div>
-                  {{ userName }}
-                </div>
-              </div>
-            </div>
-            <!--/.Card-->
-          </div>
-          <!--Grid column-->
-          <!--Grid column-->
-          <div class="col-md-8 mb-4">
-            <!--Card-->
-            <div class="card mb-4">
-              <!-- Card header -->
-              <div class="card-header text-center">
-                投稿一覧
-                <button
-                  type="button"
-                  class="btn btn-sm waves-effect float-right"
-                >
-                  <span class="fas fa-pen fa-lg" aria-hidden="true"> </span>
-                </button>
-              </div>
-              <!--Card content-->
-              <div class="card-body">
-                <div
-                  class="list-group list-group-flush"
-                  v-for="(post, i) in getPosts"
-                  :key="i"
-                >
-                  <div class="list-group-item list-group-item-action">
-                    <div>
-                      {{ post.created_at | moment }}
-                    </div>
-                    <div>
-                      {{ post.content }}
-                    </div>
+  <div>
+    <div class="container-for-admin">
+      <!--Main layout-->
+      <main class="pt-5 mx-lg-5">
+        <div class="container-fluid mt-5">
+          <!--Grid row-->
+          <div class="row wow fadeIn">
+            <!--Grid column-->
+            <div class="col-md-4 mb-4">
+              <!--Card-->
+              <div class="card text-center">
+                <!--Card content-->
+                <div class="card-body">
+                  <router-link class="text-body" to="/profile-edit">
+                    <button
+                      type="button"
+                      class="btn btn-sm waves-effect profile-edit"
+                    >
+                      編集
+                    </button>
+                  </router-link>
+                  <img :src="assetsImage" width="100" height="100" />
+                  <div>
+                    {{ userName }}
                   </div>
                 </div>
-                <paginate
-                  v-if="show"
-                  :page-count="getPageCount"
-                  :click-handler="clickCallback"
-                  :prev-text="'Prev'"
-                  :next-text="'Next'"
-                  :container-class="'pagination'"
-                  :page-class="'page-item'"
-                  :page-link-class="'page-link'"
-                  :prev-class="'page-item'"
-                  :next-class="'page-item'"
-                  :prev-link-class="'page-link'"
-                  :next-link-class="'page-link'"
-                  :active-class="'active'"
-                >
-                </paginate>
               </div>
+              <!--/.Card-->
             </div>
-            <!--/.Card-->
+            <!--Grid column-->
+            <!--Grid column-->
+            <div class="col-md-8 mb-4">
+              <!--Card-->
+              <div class="card mb-4">
+                <!-- Card header -->
+                <div class="card-header text-center">
+                  投稿一覧
+                  <button
+                    type="button"
+                    class="btn btn-sm waves-effect float-right"
+                    @click="$bvModal.show('commentModal')"
+                  >
+                    <span class="fas fa-pen fa-lg" aria-hidden="true"> </span>
+                  </button>
+                  <comment-modal @close="closeModal" />
+                </div>
+                <!--Card content-->
+                <div class="card-body">
+                  <div
+                    class="list-group list-group-flush"
+                    v-for="(post, i) in getPosts"
+                    :key="i"
+                  >
+                    <div class="list-group-item list-group-item-action">
+                      <div>
+                        {{ post.created_at | moment }}
+                      </div>
+                      <div>
+                        {{ post.content }}
+                      </div>
+                    </div>
+                  </div>
+                  <paginate
+                    v-if="paginationShow"
+                    :page-count="getPageCount"
+                    :click-handler="clickCallback"
+                    :prev-text="'Prev'"
+                    :next-text="'Next'"
+                    :container-class="'pagination'"
+                    :page-class="'page-item'"
+                    :page-link-class="'page-link'"
+                    :prev-class="'page-item'"
+                    :next-class="'page-item'"
+                    :prev-link-class="'page-link'"
+                    :next-link-class="'page-link'"
+                    :active-class="'active'"
+                  >
+                  </paginate>
+                </div>
+              </div>
+              <!--/.Card-->
+            </div>
+            <!--Grid column-->
           </div>
-          <!--Grid column-->
+          <!--Grid row-->
         </div>
-        <!--Grid row-->
-      </div>
-    </main>
-    <!--Main layout-->
+      </main>
+      <!--Main layout-->
+    </div>
   </div>
 </template>
 
@@ -91,6 +95,7 @@
 import moment from "moment";
 import store from "./store/index";
 import request from "./request";
+import CommentModal from "./CommentModal";
 import assetsImage from "@/assets/logo.png";
 
 export default {
@@ -102,8 +107,11 @@ export default {
       posts: [],
       parPage: 10,
       currentPage: 1,
-      show: false
+      paginationShow: false
     };
+  },
+  components: {
+    CommentModal
   },
   filters: {
     moment: function(date) {
@@ -118,7 +126,7 @@ export default {
       .then(userPosts => {
         this.posts = userPosts;
         if (this.posts.length > 0) {
-          this.show = true;
+          this.paginationShow = true;
         }
       })
       .catch(error => {
@@ -130,6 +138,9 @@ export default {
   methods: {
     clickCallback(pageNum) {
       this.currentPage = Number(pageNum);
+    },
+    closeModal() {
+      this.$bvModal.hide();
     }
   },
   computed: {
