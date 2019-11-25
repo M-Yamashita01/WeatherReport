0cbf83e33399cea1124ab7915956049b74e3b422
Masato Yamashita <m.yamashii@gmail.com>
2019-11-25 13:09:41 +0900

    Delete unnecessary files. #154

--- test/controllers/area/kinki_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Area::KinkiControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/factories/weathermap_locations.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :weathermap_location do
-    city_id { 1 }
-    latitude { 1.5 }
-    longitude { 1.5 }
-    city_name { "MyString" }
-  end
-end
