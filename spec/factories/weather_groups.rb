304f8d39fdbf04033affab5dcaed42bc0fbc0b67
Masato Yamashita <m.yamashii@gmail.com>
2019-11-27 13:06:06 +0900

    Add test case for WeatherGroups model. #156

---
+++ spec/factories/weather_groups.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :weather_sunny, class: WeatherGroup do
+    weather_group_id { 1 }
+    weather_icon { '01d.png' }
+    weather_main { 'Clear' }
+    weather_description { 'clear skyd' }
+  end
+end
