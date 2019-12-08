209d00e3dd6eebcf7b685a3c316b47ffa9f3779f
MYamashii <m.yamashii@gmail.com>
2019-12-08 10:46:12 +0900

    不要ファイルを削除 #155 (#157)
    * Add docker-compose for production. #154
    * Delete unnecessary files. #154
    * Change development database to production database. #154
    * Rename docker-compose.yml. #154
    * Set relative path for javascript build. #155.
    * Modify config.yml in circle ci.#155

--- test/controllers/area/kanto_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Area::KantoControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/area/tohoku_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Area::TohokuControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
