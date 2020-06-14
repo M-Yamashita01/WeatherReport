993a204855fc28d8a68d8dfa5057a78587cd82b2
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-06-14 23:30:28 +0900

    Create script for pushing image to AWS by manual. #195

---
+++ deploy/pushImage/PushMigrationImageForECR.sh
@@ -0,0 +1,8 @@
+aws ecr get-login-password --profile my-profile-name | \
+docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/weatherreport_app:migration_v1.0 \
+             -f $(git rev-parse --show-toplevel)/backend/Dockerfile.migration \
+             $(git rev-parse --show-toplevel)/backend/
+
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/weatherreport_app:backend_v1.0
