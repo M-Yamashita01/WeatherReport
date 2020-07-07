bd629c89ddf0d78dcafe5c823064b968e8eeb7dd
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-07-07 21:38:01 +0900

    Apply manifest files in circleci. #213

--- deploy/k8s/apply.sh
+++ deploy/k8s/apply.sh
@@ -1,12 +1,12 @@
-envsubst '$$RDS_ENDPOINT' < ./k8s/config/rails_config.yaml > ./k8s/config/rails_config_converted.yaml
-envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./k8s/settings/set_db_job.yaml >  ./k8s/settings/set_db_job_converted.yaml
-envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./k8s/deployment.yaml >  ./k8s/deployment_converted.yaml
+envsubst '$$RDS_ENDPOINT' < ./config/rails_config.yaml > ./config/rails_config_converted.yaml
+envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
+envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
 
 aws eks update-kubeconfig --name weatherreport-prod-cluster
 
-kubectl apply -f ./k8s/config/rails_config_converted.yaml
-kubectl apply -f ./k8s/config/vue_config.yaml
+kubectl apply -f ./config/rails_config_converted.yaml
+kubectl apply -f ./config/vue_config.yaml
 
-kubectl apply -f ./k8s/settings/set_db_job_converted.yaml
+kubectl apply -f ./settings/set_db_job_converted.yaml
 
-kubectl apply -f ./k8s/deployment_converted.yaml
+kubectl apply -f ./deployment_converted.yaml
