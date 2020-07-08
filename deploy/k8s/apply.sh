79b1c6dda5c9f0a5ab71c10140922343b549f243
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-07-08 19:37:58 +0900

    Replace environment variable names. And get rds endpoint by aws command and assign it to RDS_EDNPOINT. #213

--- deploy/k8s/apply.sh
+++ deploy/k8s/apply.sh
@@ -1,6 +1,10 @@
+aws rds --region ap-northeast-1 describe-db-instances --query "DBInstances[*].Endpoint.Address" --output text > tmpfile
+export RDS_ENDPOINT=$(cat tmpfile)
+rm tmpfile
+
 envsubst '$$RDS_ENDPOINT' < ./config/rails_config.yaml > ./config/rails_config_converted.yaml
-envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
-envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
+envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
+envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
 
 aws eks update-kubeconfig --name weatherreport-prod-cluster
 
--- deploy/k8s/delete.sh
+++ deploy/k8s/delete.sh
@@ -1,6 +1,8 @@
+export RDS_ENDPOINT=`aws rds --region $AWS_REGION describe-db-instances --query "DBInstances[*].Endpoint.Address" --output text` 
+
 envsubst '$$RDS_ENDPOINT' < ./config/rails_config.yaml > ./config/rails_config_converted.yaml
-envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
-envsubst '$$AWS_ACCOUNT_ID $$AWS_DEFAULT_REGION $$AWS_ECR_REPOSITORY_NAME $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
+envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
+envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
 
 aws eks update-kubeconfig --name weatherreport-prod-cluster
 
