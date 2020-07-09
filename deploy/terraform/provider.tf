d05e0394affd5aef245158624b7f9d055e7e104d
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-07-09 21:50:22 +0900

    Delete unnecessary codes. #215

--- deploy/terraform/provider.tf
+++ deploy/terraform/provider.tf
@@ -1,6 +1,4 @@
 provider "aws" {
   version = "~> 2.0"
-  profile                 = "my-profile-name"
-  shared_credentials_file = "/Users/masatoyamashita/.aws/credentials"
   region = "ap-northeast-1"
 }
