65c083d464b962f67988f0deaa7edfc047873922
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-07-23 13:10:08 +0900

    Add terraform init, plan and apply command in config.yml for CI/CD. #215

--- deploy/terraform/provider.tf
+++ deploy/terraform/provider.tf
@@ -1,4 +1,16 @@
 provider "aws" {
-  version = "~> 2.0"
+  access_key = "$AWS_ACCESS_KEY_ID"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
   region = "ap-northeast-1"
+}
+
+terraform {
+  backend "s3" {
+    access_key = "$AWS_ACCESS_KEY_ID"
+    secret_key = "$AWS_SECRET_ACCESS_KEY"
+    bucket  = "$AWS_BUCKET"
+    key     = "terraform.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
+  }
 }
