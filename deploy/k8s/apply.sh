8b515e29202d389a77b2ffb2de5f1c2c451ac2af
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-07-24 18:11:41 +0900

    Add region in apply.sh. Remove debug codes. #223

--- deploy/k8s/apply.sh
+++ deploy/k8s/apply.sh
@@ -1,21 +1,12 @@
-echo "debug"
-aws rds --region ap-northeast-1 describe-db-instances --query "DBInstances[*].Endpoint.Address"
-echo "debug1 start"
-aws rds --region ap-northeast-1 describe-db-instances --query "DBInstances[*].Endpoint.Address"
-echo "debug1 end"
-
 aws rds --region ap-northeast-1 describe-db-instances --query "DBInstances[*].Endpoint.Address" --output text > tmpfile
 export RDS_ENDPOINT=$(cat tmpfile)
-echo "debug2 start"
-env
-echo "debug2 end"
 rm tmpfile
 
 envsubst '$$RDS_ENDPOINT' < ./config/rails_config.yaml > ./config/rails_config_converted.yaml
 envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
 envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
 
-aws eks update-kubeconfig --name weatherreport-prod-cluster
+aws eks --region ap-northeast-1 update-kubeconfig --name weatherreport-prod-cluster
 
 kubectl apply -f ./config/rails_config_converted.yaml
 kubectl apply -f ./config/vue_config.yaml
