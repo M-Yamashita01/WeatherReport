55786aa5c5beea32dff9fdc9a2d7cd09890a8497
M.Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-07-25 17:55:32 +0900

    #223 マニフェストファイル適用時にawsコマンドのregion指定がされていない (#224)
    * Add region in apply.sh. Remove debug codes. #223
    * Rename tag in aws-ecr/build-and-push-image job. #223

--- deploy/k8s/apply.sh
+++ deploy/k8s/apply.sh
@@ -6,7 +6,7 @@ envsubst '$$RDS_ENDPOINT' < ./config/rails_config.yaml > ./config/rails_config_c
 envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
 envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
 
-aws eks update-kubeconfig --name weatherreport-prod-cluster
+aws eks --region ap-northeast-1 update-kubeconfig --name weatherreport-prod-cluster
 
 kubectl apply -f ./config/rails_config_converted.yaml
 kubectl apply -f ./config/vue_config.yaml
