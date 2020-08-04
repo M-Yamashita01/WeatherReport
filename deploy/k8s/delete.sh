29ff97eb2b61a1c91a446236bee4e203463d0706
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-08-04 23:09:02 +0900

    Replace mysql user and password to environments. #237

--- deploy/k8s/delete.sh
+++ deploy/k8s/delete.sh
@@ -6,7 +6,7 @@ aws iam list-roles --region ap-northeast-1 --query "Roles[?RoleName == 'route53-
 export ROUTE53_EXDNS_CONTROLLER_ARN=$(cat tmpfile)
 rm tmpfile
 
-envsubst '$$RDS_ENDPOINT' < ./config/rails_config.yaml > ./config/rails_config_converted.yaml
+envsubst '$$RDS_ENDPOINT $$DB_USERNAME $$DB_PASSWORD' < ./config/rails_config.yaml > ./config/rails_config_converted.yaml
 envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./settings/set_db_job.yaml >  ./settings/set_db_job_converted.yaml
 envsubst '$$AWS_ACCOUNT_ID $$AWS_REGION $$AWS_ECR_REPOSITORY $$FRONTEND_IMAGE_NAME $$FRONTEND_IMAGE_VERSION $$BACKEND_IMAGE_NAME $$BACKEND_IMAGE_VERSION' < ./deployment.yaml >  ./deployment_converted.yaml
 envsubst '$$ROUTE53_EXDNS_CONTROLLER_ARN' < ./external-dns-manifest.yaml > ./external-dns-manifest_converted.yaml
