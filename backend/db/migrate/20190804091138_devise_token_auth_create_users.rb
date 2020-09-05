65347a6d3be18abb57cd142d46fd693f2845a8c4
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-09-05 18:37:55 +0900

    Resolve rubocop error. #200

--- backend/db/migrate/20190804091138_devise_token_auth_create_users.rb
+++ backend/db/migrate/20190804091138_devise_token_auth_create_users.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table(:users) do |t|
--- backend/db/migrate/20191116121207_add_japanese_location_name_to_weathermap_locations.rb
+++ backend/db/migrate/20191116121207_add_japanese_location_name_to_weathermap_locations.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# class AddJapaneseLocationNameToWeathermapLocations
+#                               < ActiveRecord::Migration[5.2]
 class AddJapaneseLocationNameToWeathermapLocations < ActiveRecord::Migration[5.2]
   def change
     add_column :weathermap_locations, :city_name_ja, :string
