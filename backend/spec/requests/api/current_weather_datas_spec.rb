94da4db072fe6d535058acb4c176d74242aa90bc
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-09-11 23:02:55 +0900

    Add context statement in rspec. #200

--- backend/spec/requests/api/current_weather_datas_spec.rb
+++ backend/spec/requests/api/current_weather_datas_spec.rb
@@ -3,29 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe 'CurrentWeatherDatas', type: :request do
-  before(:each) do
-    @weathermap_location = FactoryBot.create(:city)
-    @weather_group = FactoryBot.create(:weather_sunny)
-    @current_weather = FactoryBot.create(:current_weather_sample, weathermap_location: @weathermap_location, weather_group: @weather_group)
-  end
-
   describe 'GET /current_weather_datas' do
-    it 'works! (now write some real specs)' do
-      get '/api/current_weather_datas'
-      expect(response).to have_http_status(200)
+    context '登録されている地域の天気が1つのみの場合' do
+      let(:weathermap_location) { create(:city) }
+      let(:weather_group) { create(:weather_sunny) }
+      let!(:current_weather) { create(:current_weather_sample, weathermap_location: weathermap_location, weather_group: weather_group) }
 
-      json = JSON.parse(response.body)
-      expect(json['current_weather_data'].length).to eq 1
+      it '地域の天気1件のみ取得できること' do
+        get '/api/current_weather_datas'
+        expect(response).to have_http_status(200)
+
+        json = JSON.parse(response.body)
+        expect(json['current_weather_data'].length).to eq 1
+      end
     end
   end
 
   describe 'GET /current_weather_datas?longitude_max=&longitude_min=&latitude_max=&latitude_min=' do
-    it 'works! (now write some real specs)' do
-      get '/api/current_weather_datas?longitude_max=&longitude_min=&latitude_max=&latitude_min='
-      expect(response).to have_http_status(200)
+    context '登録されている地域の天気が1つのみで、緯度経度を指定せずに天気を取得した場合' do
+      let(:weathermap_location) { create(:city) }
+      let(:weather_group) { create(:weather_sunny) }
+      let!(:current_weather) { create(:current_weather_sample, weathermap_location: weathermap_location, weather_group: weather_group) }
 
-      json = JSON.parse(response.body)
-      expect(json['current_weather_data'].length).to eq 1
+      it '地域の天気1件のみ取得できること' do
+        get '/api/current_weather_datas?longitude_max=&longitude_min=&latitude_max=&latitude_min='
+        expect(response).to have_http_status(200)
+
+        json = JSON.parse(response.body)
+        expect(json['current_weather_data'].length).to eq 1
+      end
     end
   end
 end
