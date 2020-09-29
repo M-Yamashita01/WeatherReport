7319e923dea305c40b182b34c3e73aacd72f0ffe
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-09-29 22:41:52 +0900

    Create scope to search location using latitude and longitude. #256

--- backend/spec/models/weathermap_location_spec.rb
+++ backend/spec/models/weathermap_location_spec.rb
@@ -3,48 +3,126 @@
 require 'rails_helper'
 
 RSpec.describe WeathermapLocation, type: :model do
-  before(:each) do
-    @weather_location = FactoryBot.create(:city)
-  end
+  describe 'validation' do
+    context '地域情報が存在する場合' do
+      let(:location) { create(:tokyo) }
 
-  describe '地域情報が存在する場合' do
-    it 'validationがpassすること' do
-      expect(@weather_location).to be_valid
+      it 'validationがpassすること' do
+        expect(location).to be_valid
+      end
+    end
+
+    context '地域情報の地域idが存在しない場合' do
+      let(:location) { create(:tokyo) }
+
+      it 'validationがpassしないこと' do
+        location.city_id = nil
+        expect(location).not_to be_valid
+      end
+    end
+
+    context '地域情報の緯度が存在しない場合' do
+      let(:location) { create(:tokyo) }
+
+      it 'validationがpassしないこと' do
+        location.latitude = nil
+        expect(location).not_to be_valid
+      end
+    end
+
+    context '地域情報の経度が存在しない場合' do
+      let(:location) { create(:tokyo) }
+
+      it 'validationがpassしないこと' do
+        location.longitude = nil
+        expect(location).not_to be_valid
+      end
+    end
+
+    context '地域情報の英語名が存在しない場合' do
+      let(:location) { create(:tokyo) }
+
+      it 'validationがpassしないこと' do
+        location.city_name = ''
+        expect(location).not_to be_valid
+      end
+    end
+
+    context '地域情報の日本語名が存在しない場合' do
+      let(:location) { create(:tokyo) }
+
+      it 'validationがpassすること' do
+        location.city_name_ja = ''
+        expect(location).to be_valid
+      end
     end
   end
 
-  describe '地域情報の地域idが存在しない場合' do
-    it 'validationがpassしないこと' do
-      @weather_location.city_id = nil
-      expect(@weather_location).not_to be_valid
-    end
-  end
+  describe 'scope' do
+    describe '#search_location' do
+      let!(:location_tokyo) { create(:tokyo) }
+      let!(:location_osaka) { create(:osaka) }
 
-  describe '地域情報の緯度が存在しない場合' do
-    it 'validationがpassしないこと' do
-      @weather_location.latitude = nil
-      expect(@weather_location).not_to be_valid
-    end
-  end
+      subject { WeathermapLocation.search_location(latitude, longitude) }
 
-  describe '地域情報の経度が存在しない場合' do
-    it 'validationがpassしないこと' do
-      @weather_location.longitude = nil
-      expect(@weather_location).not_to be_valid
-    end
-  end
+      context 'テーブルに存在する緯度、経度が与えられた場合' do
+        let(:latitude) { 139.691 }
+        let(:longitude) { 35.689 }
 
-  describe '地域情報の英語名が存在しない場合' do
-    it 'validationがpassしないこと' do
-      @weather_location.city_name = ''
-      expect(@weather_location).not_to be_valid
-    end
-  end
+        it '取得したレコードが1件であること' do
+          expect(subject.count).to eq(1)
+        end
 
-  describe '地域情報の日本語名が存在しない場合' do
-    it 'validationがpassすること' do
-      @weather_location.city_name_ja = ''
-      expect(@weather_location).to be_valid
+        it '取得したレコードの緯度、経度が一致していること' do
+          expect(subject[0].latitude.to_digits).to eq("139.691")
+          expect(subject[0].longitude.to_digits).to eq("35.689")
+        end
+      end
+
+      context 'テーブルに存在しない緯度、経度が与えられた場合' do
+        let(:latitude) { 0 }
+        let(:longitude) { 0 }
+
+        it '取得したレコードが0件であること' do
+          expect(subject.count).to eq(0)
+        end
+      end
+
+      context '緯度、経度が両方空文字の場合' do
+        let(:latitude) { "" }
+        let(:longitude) { "" }
+
+        it '取得したレコードが0件であること' do
+          expect(subject.count).to eq(0)
+        end
+      end
+
+      context '緯度のみ片方空文字の場合' do
+        let(:latitude) { "" }
+        let(:longitude) { 0 }
+
+        it '取得したレコードが0件であること' do
+          expect(subject.count).to eq(0)
+        end
+      end
+
+      context '経度のみ片方空文字の場合' do
+        let(:latitude) { 0 }
+        let(:longitude) { "" }
+
+        it '取得したレコードが0件であること' do
+          expect(subject.count).to eq(0)
+        end
+      end
+
+      context '緯度、経度が適当な文字の場合' do
+        let(:latitude) { "hogehoge" }
+        let(:longitude) { "hogehoge" }
+
+        it '取得したレコードが0件であること' do
+          expect(subject.count).to eq(0)
+        end
+      end
     end
   end
 end
